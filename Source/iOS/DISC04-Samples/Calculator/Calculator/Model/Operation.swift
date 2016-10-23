64fb7c08714f07a507663b1282e0cb953f4addff
Rafael Veronezi <rafael@caeno.io>
2016-10-23 17:59:31 -0200

    Ajustes e adição de comentários detalhados ao projeto da Calculadora.

--- Source/iOS/DISC04-Samples/Calculator/Calculator/Model/Operation.swift
+++ Source/iOS/DISC04-Samples/Calculator/Calculator/Model/Operation.swift
@@ -2,15 +2,27 @@
 //  Operation.swift
 //  Calculator
 //
-//  Created by Hakan Satya on 21/10/16.
+//  Created by Rafael Veronezi on 21/10/16.
 //  Copyright © 2016 Caeno. All rights reserved.
 //
 
 import Foundation
 
+/**
+ Enumera as operações que podem ser realizadas pela versão atual do serviço de Calculadora (classe **_CalculatorService_**). Essa enumeração utiliza números inteiros para identificar seus elementos, de forma que possamos usar esses códigos para referenciar a enumeração quando ela for utilizada.
+ */
 enum Operation : Int {
+    
+    /// Operação de Soma
     case sum = 1
+    
+    /// Operação de Subtração
     case subtract = 2
+    
+    // Operação de Multiplicação
     case multiply = 3
+    
+    /// Operação de Divisão
     case divide = 4
+    
 }
--- Source/iOS/DISC04-Samples/Calculator/Calculator/ViewModels/CalculatorViewController.swift
+++ Source/iOS/DISC04-Samples/Calculator/Calculator/ViewModels/CalculatorViewController.swift
@@ -2,22 +2,30 @@
 //  CalculatorViewController.swift
 //  Calculator
 //
-//  Created by Hakan Satya on 21/10/16.
+//  Created by Rafael Veronezi on 21/10/16.
 //  Copyright © 2016 Caeno. All rights reserved.
 //
 
 import UIKit
 
+/**
+ View Controller usado para gerenciar a tela principal da Calculadora.
+ 
+ - author: Rafael Veronezi
+ - version: 1.0.0
+ */
 class CalculatorViewController: UIViewController {
 
     //
     // MARK: - Properties
     
+    /// Modelo utilizado pelo View Controller para realizar as operações da Calculadora
     var calcService = CalculatorService()
     
     //
     // MARK: - Outlets
     
+    /// Um outlet para o componente _UITextField_ que representa o display numérico da Calculadora
     @IBOutlet weak var displayTextField: UITextField!
     
     //
@@ -26,7 +34,7 @@ class CalculatorViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        // Configra o Modelo para que o display numérico da calculadora seja atualizado com o valor do total sempre que este for modificado.
         calcService.totalChangedCallback = { value in
             self.displayTextField.text = String(value)
         }
@@ -35,22 +43,36 @@ class CalculatorViewController: UIViewController {
     //
     // MARK: - Action Methods
     
+    /// Action Method disparado quando algum dos digitos numéricos é pressionado
     @IBAction func typeDigitTouched(_ sender: UIButton) {
-        let digit = Int(sender.title(for: .normal)!)!
-        calcService.addDigit(digit)
+        // Extrai o digito do título atual do botão e tenta transforma ele em um valor Int.
+        if let title = sender.currentTitle, let digit = Int(title) {
+            try! calcService.addDigit(digit)
+        }
     }
     
+    // Action Method disparado quando uma operação é pressionada
     @IBAction func typeOperationTouched(_ sender: UIButton) {
         let operation = Operation(rawValue: sender.tag)!
-        calcService.set(operation: operation)
+        calcService.setOperation(operation)
     }
     
+    // Action Method disparado quando o botão Limpar é pressionado
     @IBAction func clearTouched(_ sender: UIButton) {
         calcService.clear()
     }
     
+    // Action Method disparado quando o botão de igual é pressionado
     @IBAction func equalsTouched(_ sender: UIButton) {
-        calcService.performEquals()
+        do {
+            try calcService.performOperation()
+        } catch CalculatorServiceError.DivisionByZero {
+            let alert = UIAlertController(title: "Calculadora", message: "Você não pode dividir o número por 0.", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+            present(alert, animated: true, completion: nil)
+        } catch {
+            // Ignora outros erros
+        }
     }
     
 }
