6ee919078bd865b86e0c72d3c08f77c3d0484034
Rafael Veronezi <rafael@caeno.io>
2016-11-19 13:31:55 -0200

    Ajustes no projeto inicial da prática Countries para Android.

--- Source/Android/DISC06-Samples/Countries-Starter/app/src/main/java/io/caeno/countries/MainActivity.java
+++ Source/Android/DISC06-Samples/Countries-Starter/app/src/main/java/io/caeno/countries/MainActivity.java
@@ -3,6 +3,7 @@ package io.caeno.countries;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -22,6 +23,9 @@ public class MainActivity extends AppCompatActivity {
 
     CountryList mCountryList;
     ListView mCountriesListView;
+
+    //
+    // ACTIVITY LIFE CYCLE
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +50,13 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 
     //
